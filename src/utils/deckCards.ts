ffe9122dce299098490bcc8d7de37af18a108e3e
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-30 12:18:31 -0300

    add: deck cards

---
+++ src/utils/deckCards.ts
@@ -0,0 +1,318 @@
+import copas_a from '../assets/copas/copas_a.png';
+import copas_2 from '../assets/copas/copas_2.png';
+import copas_3 from '../assets/copas/copas_3.png';
+import copas_4 from '../assets/copas/copas_4.png';
+import copas_5 from '../assets/copas/copas_5.png';
+import copas_6 from '../assets/copas/copas_6.png';
+import copas_7 from '../assets/copas/copas_7.png';
+import copas_8 from '../assets/copas/copas_8.png';
+import copas_9 from '../assets/copas/copas_a.png';
+import copas_10 from '../assets/copas/copas_10.png';
+import copas_j from '../assets/copas/copas_j.png';
+import copas_q from '../assets/copas/copas_k.png';
+import copas_k from '../assets/copas/copas_a.png';
+
+import espadas_a from '../assets/espadas/espadas_a.png';
+import espadas_2 from '../assets/espadas/espadas_2.png';
+import espadas_3 from '../assets/espadas/espadas_3.png';
+import espadas_4 from '../assets/espadas/espadas_4.png';
+import espadas_5 from '../assets/espadas/espadas_5.png';
+import espadas_6 from '../assets/espadas/espadas_6.png';
+import espadas_7 from '../assets/espadas/espadas_7.png';
+import espadas_8 from '../assets/espadas/espadas_8.png';
+import espadas_9 from '../assets/espadas/espadas_a.png';
+import espadas_10 from '../assets/espadas/espadas_10.png';
+import espadas_j from '../assets/espadas/espadas_j.png';
+import espadas_q from '../assets/espadas/espadas_k.png';
+import espadas_k from '../assets/espadas/espadas_a.png';
+
+import ouros_a from '../assets/ouros/ouros_a.png';
+import ouros_2 from '../assets/ouros/ouros_2.png';
+import ouros_3 from '../assets/ouros/ouros_3.png';
+import ouros_4 from '../assets/ouros/ouros_4.png';
+import ouros_5 from '../assets/ouros/ouros_5.png';
+import ouros_6 from '../assets/ouros/ouros_6.png';
+import ouros_7 from '../assets/ouros/ouros_7.png';
+import ouros_8 from '../assets/ouros/ouros_8.png';
+import ouros_9 from '../assets/ouros/ouros_a.png';
+import ouros_10 from '../assets/ouros/ouros_10.png';
+import ouros_j from '../assets/ouros/ouros_j.png';
+import ouros_q from '../assets/ouros/ouros_k.png';
+import ouros_k from '../assets/ouros/ouros_a.png';
+
+import paus_a from '../assets/paus/paus_a.png';
+import paus_2 from '../assets/paus/paus_2.png';
+import paus_3 from '../assets/paus/paus_3.png';
+import paus_4 from '../assets/paus/paus_4.png';
+import paus_5 from '../assets/paus/paus_5.png';
+import paus_6 from '../assets/paus/paus_6.png';
+import paus_7 from '../assets/paus/paus_7.png';
+import paus_8 from '../assets/paus/paus_8.png';
+import paus_9 from '../assets/paus/paus_a.png';
+import paus_10 from '../assets/paus/paus_10.png';
+import paus_j from '../assets/paus/paus_j.png';
+import paus_q from '../assets/paus/paus_k.png';
+import paus_k from '../assets/paus/paus_a.png';
+
+export default [
+  {
+    id: 0,
+    icon: copas_a,
+    description: 'copas_a',
+  },
+  {
+    id: 1,
+    icon: copas_2,
+    description: 'copas_2',
+  },
+  {
+    id: 2,
+    icon: copas_3,
+    description: 'copas_3',
+  },
+  {
+    id: 3,
+    icon: copas_4,
+    description: 'copas_4',
+  },
+  {
+    id: 4,
+    icon: copas_5,
+    description: 'copas_5',
+  },
+  {
+    id: 5,
+    icon: copas_6,
+    description: 'copas_6',
+  },
+  {
+    id: 6,
+    icon: copas_7,
+    description: 'copas_7',
+  },
+  {
+    id: 7,
+    icon: copas_8,
+    description: 'copas_8',
+  },
+  {
+    id: 8,
+    icon: copas_9,
+    description: 'copas_9',
+  },
+  {
+    id: 9,
+    icon: copas_10,
+    description: 'copas_10',
+  },
+  {
+    id: 10,
+    icon: copas_j,
+    description: 'copas_j',
+  },
+  {
+    id: 11,
+    icon: copas_q,
+    description: 'copas_q',
+  },
+  {
+    id: 12,
+    icon: copas_k,
+    description: 'copas_k',
+  },
+  {
+    id: 13,
+    icon: espadas_a,
+    description: 'espadas_a',
+  },
+  {
+    id: 14,
+    icon: espadas_2,
+    description: 'espadas_2',
+  },
+  {
+    id: 15,
+    icon: espadas_3,
+    description: 'espadas_3',
+  },
+  {
+    id: 16,
+    icon: espadas_4,
+    description: 'espadas_4',
+  },
+  {
+    id: 17,
+    icon: espadas_5,
+    description: 'espadas_5',
+  },
+  {
+    id: 18,
+    icon: espadas_6,
+    description: 'espadas_6',
+  },
+  {
+    id: 19,
+    icon: espadas_7,
+    description: 'espadas_7',
+  },
+  {
+    id: 20,
+    icon: espadas_8,
+    description: 'espadas_8',
+  },
+  {
+    id: 21,
+    icon: espadas_9,
+    description: 'espadas_9',
+  },
+  {
+    id: 22,
+    icon: espadas_10,
+    description: 'espadas_10',
+  },
+  {
+    id: 23,
+    icon: espadas_j,
+    description: 'espadas_j',
+  },
+  {
+    id: 24,
+    icon: espadas_q,
+    description: 'espadas_q',
+  },
+  {
+    id: 25,
+    icon: espadas_k,
+    description: 'espadas_k',
+  },
+  {
+    id: 26,
+    icon: ouros_a,
+    description: 'ouros_a',
+  },
+  {
+    id: 27,
+    icon: ouros_2,
+    description: 'ouros_2',
+  },
+  {
+    id: 28,
+    icon: ouros_3,
+    description: 'ouros_3',
+  },
+  {
+    id: 29,
+    icon: ouros_4,
+    description: 'ouros_4',
+  },
+  {
+    id: 30,
+    icon: ouros_5,
+    description: 'ouros_5',
+  },
+  {
+    id: 31,
+    icon: ouros_6,
+    description: 'ouros_6',
+  },
+  {
+    id: 32,
+    icon: ouros_7,
+    description: 'ouros_7',
+  },
+  {
+    id: 33,
+    icon: ouros_8,
+    description: 'ouros_8',
+  },
+  {
+    id: 34,
+    icon: ouros_9,
+    description: 'ouros_9',
+  },
+  {
+    id: 35,
+    icon: ouros_10,
+    description: 'ouros_10',
+  },
+  {
+    id: 36,
+    icon: ouros_j,
+    description: 'ouros_j',
+  },
+  {
+    id: 37,
+    icon: ouros_q,
+    description: 'ouros_q',
+  },
+  {
+    id: 38,
+    icon: ouros_k,
+    description: 'ouros_k',
+  },
+  {
+    id: 39,
+    icon: paus_a,
+    description: 'paus_a',
+  },
+  {
+    id: 40,
+    icon: paus_2,
+    description: 'paus_2',
+  },
+  {
+    id: 41,
+    icon: paus_3,
+    description: 'paus_3',
+  },
+  {
+    id: 42,
+    icon: paus_4,
+    description: 'paus_4',
+  },
+  {
+    id: 43,
+    icon: paus_5,
+    description: 'paus_5',
+  },
+  {
+    id: 44,
+    icon: paus_6,
+    description: 'paus_6',
+  },
+  {
+    id: 45,
+    icon: paus_7,
+    description: 'paus_7',
+  },
+  {
+    id: 46,
+    icon: paus_8,
+    description: 'paus_8',
+  },
+  {
+    id: 47,
+    icon: paus_9,
+    description: 'paus_9',
+  },
+  {
+    id: 48,
+    icon: paus_10,
+    description: 'paus_10',
+  },
+  {
+    id: 49,
+    icon: paus_j,
+    description: 'paus_j',
+  },
+  {
+    id: 50,
+    icon: paus_q,
+    description: 'paus_q',
+  },
+  {
+    id: 51,
+    icon: paus_k,
+    description: 'paus_k',
+  },
+];
