9b5466193fcc42d9e4791741936e8964d3dae906
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-30 11:06:40 -0300

    add: cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
