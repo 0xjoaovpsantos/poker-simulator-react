5befdb355c5c4aeaf1e10b5d9e8ad43c6b762c27
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-30 11:18:28 -0300

    add: create global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root'),
 );
