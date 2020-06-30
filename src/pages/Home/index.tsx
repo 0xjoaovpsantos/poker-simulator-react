849a90b4d1dbc49051a65cec3ec41e1669eeb62b
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-30 11:47:31 -0300

    add: cards to home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,119 @@
 import React from 'react';
 
-const Home: React.FC = () => <p>Home</p>;
+import copas_a from '../../assets/copas/copas_a.png';
+import copas_2 from '../../assets/copas/copas_2.png';
+import copas_3 from '../../assets/copas/copas_3.png';
+import copas_4 from '../../assets/copas/copas_4.png';
+import copas_5 from '../../assets/copas/copas_5.png';
+import copas_6 from '../../assets/copas/copas_6.png';
+import copas_7 from '../../assets/copas/copas_7.png';
+import copas_8 from '../../assets/copas/copas_8.png';
+import copas_9 from '../../assets/copas/copas_a.png';
+import copas_10 from '../../assets/copas/copas_10.png';
+import copas_j from '../../assets/copas/copas_j.png';
+import copas_q from '../../assets/copas/copas_k.png';
+import copas_k from '../../assets/copas/copas_a.png';
+
+import espadas_a from '../../assets/espadas/espadas_a.png';
+import espadas_2 from '../../assets/espadas/espadas_2.png';
+import espadas_3 from '../../assets/espadas/espadas_3.png';
+import espadas_4 from '../../assets/espadas/espadas_4.png';
+import espadas_5 from '../../assets/espadas/espadas_5.png';
+import espadas_6 from '../../assets/espadas/espadas_6.png';
+import espadas_7 from '../../assets/espadas/espadas_7.png';
+import espadas_8 from '../../assets/espadas/espadas_8.png';
+import espadas_9 from '../../assets/espadas/espadas_a.png';
+import espadas_10 from '../../assets/espadas/espadas_10.png';
+import espadas_j from '../../assets/espadas/espadas_j.png';
+import espadas_q from '../../assets/espadas/espadas_k.png';
+import espadas_k from '../../assets/espadas/espadas_a.png';
+
+import ouros_a from '../../assets/ouros/ouros_a.png';
+import ouros_2 from '../../assets/ouros/ouros_2.png';
+import ouros_3 from '../../assets/ouros/ouros_3.png';
+import ouros_4 from '../../assets/ouros/ouros_4.png';
+import ouros_5 from '../../assets/ouros/ouros_5.png';
+import ouros_6 from '../../assets/ouros/ouros_6.png';
+import ouros_7 from '../../assets/ouros/ouros_7.png';
+import ouros_8 from '../../assets/ouros/ouros_8.png';
+import ouros_9 from '../../assets/ouros/ouros_a.png';
+import ouros_10 from '../../assets/ouros/ouros_10.png';
+import ouros_j from '../../assets/ouros/ouros_j.png';
+import ouros_q from '../../assets/ouros/ouros_k.png';
+import ouros_k from '../../assets/ouros/ouros_a.png';
+
+import paus_a from '../../assets/paus/paus_a.png';
+import paus_2 from '../../assets/paus/paus_2.png';
+import paus_3 from '../../assets/paus/paus_3.png';
+import paus_4 from '../../assets/paus/paus_4.png';
+import paus_5 from '../../assets/paus/paus_5.png';
+import paus_6 from '../../assets/paus/paus_6.png';
+import paus_7 from '../../assets/paus/paus_7.png';
+import paus_8 from '../../assets/paus/paus_8.png';
+import paus_9 from '../../assets/paus/paus_a.png';
+import paus_10 from '../../assets/paus/paus_10.png';
+import paus_j from '../../assets/paus/paus_j.png';
+import paus_q from '../../assets/paus/paus_k.png';
+import paus_k from '../../assets/paus/paus_a.png';
+
+const Home: React.FC = () => (
+  <>
+    <img src={copas_a} alt="Cartas Copas A" />
+    <img src={copas_2} alt="Cartas Copas 2" />
+    <img src={copas_3} alt="Cartas Copas 3" />
+    <img src={copas_4} alt="Cartas Copas 4" />
+    <img src={copas_5} alt="Cartas Copas 5" />
+    <img src={copas_6} alt="Cartas Copas 6" />
+    <img src={copas_7} alt="Cartas Copas 7" />
+    <img src={copas_8} alt="Cartas Copas 8" />
+    <img src={copas_9} alt="Cartas Copas 9" />
+    <img src={copas_10} alt="Cartas Copas 10" />
+    <img src={copas_j} alt="Cartas Copas J" />
+    <img src={copas_q} alt="Cartas Copas Q" />
+    <img src={copas_k} alt="Cartas Copas K" />
+
+    <img src={espadas_a} alt="Cartas espadas A" />
+    <img src={espadas_2} alt="Cartas espadas 2" />
+    <img src={espadas_3} alt="Cartas espadas 3" />
+    <img src={espadas_4} alt="Cartas espadas 4" />
+    <img src={espadas_5} alt="Cartas espadas 5" />
+    <img src={espadas_6} alt="Cartas espadas 6" />
+    <img src={espadas_7} alt="Cartas espadas 7" />
+    <img src={espadas_8} alt="Cartas espadas 8" />
+    <img src={espadas_9} alt="Cartas espadas 9" />
+    <img src={espadas_10} alt="Cartas espadas 10" />
+    <img src={espadas_j} alt="Cartas espadas J" />
+    <img src={espadas_q} alt="Cartas espadas Q" />
+    <img src={espadas_k} alt="Cartas espadas K" />
+
+    <img src={ouros_a} alt="Cartas ouros A" />
+    <img src={ouros_2} alt="Cartas ouros 2" />
+    <img src={ouros_3} alt="Cartas ouros 3" />
+    <img src={ouros_4} alt="Cartas ouros 4" />
+    <img src={ouros_5} alt="Cartas ouros 5" />
+    <img src={ouros_6} alt="Cartas ouros 6" />
+    <img src={ouros_7} alt="Cartas ouros 7" />
+    <img src={ouros_8} alt="Cartas ouros 8" />
+    <img src={ouros_9} alt="Cartas ouros 9" />
+    <img src={ouros_10} alt="Cartas ouros 10" />
+    <img src={ouros_j} alt="Cartas ouros J" />
+    <img src={ouros_q} alt="Cartas ouros Q" />
+    <img src={ouros_k} alt="Cartas ouros K" />
+
+    <img src={paus_a} alt="Cartas paus A" />
+    <img src={paus_2} alt="Cartas paus 2" />
+    <img src={paus_3} alt="Cartas paus 3" />
+    <img src={paus_4} alt="Cartas paus 4" />
+    <img src={paus_5} alt="Cartas paus 5" />
+    <img src={paus_6} alt="Cartas paus 6" />
+    <img src={paus_7} alt="Cartas paus 7" />
+    <img src={paus_8} alt="Cartas paus 8" />
+    <img src={paus_9} alt="Cartas paus 9" />
+    <img src={paus_10} alt="Cartas paus 10" />
+    <img src={paus_j} alt="Cartas paus J" />
+    <img src={paus_q} alt="Cartas paus Q" />
+    <img src={paus_k} alt="Cartas paus K" />
+  </>
+);
 
 export default Home;
