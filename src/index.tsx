211ecf3b2dc32404723d1269ec20660299e28fc1
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-07-01 03:14:47 -0300

    deploy: to github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Routes from './routes';
+import Routes from './routes/';
 import GlobalStyle from './styles/global';
 
 ReactDOM.render(
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
+import Home from '../pages/Home';
 
 const Routes: React.FC = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Route component={Home} path="/" />
   </BrowserRouter>
 );
