71e381e8f8fb17c9134d822c3442e8889cc01e08
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-07-01 03:08:52 -0300

    Updates

---
+++ precache-manifest.3b1889b1545208aa496a0dcc283817cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181063046c2c8331291da2eeb1d42e71",
+    "url": "/poker-simulator-react/index.html"
+  },
+  {
+    "revision": "23e622467bd1a682c14e",
+    "url": "/poker-simulator-react/static/js/2.d34cb2d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poker-simulator-react/static/js/2.d34cb2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f02f97db28f376edd95",
+    "url": "/poker-simulator-react/static/js/main.d913de05.chunk.js"
+  },
+  {
+    "revision": "7db912740c4d3d0129c0",
+    "url": "/poker-simulator-react/static/js/runtime-main.6550f7a9.js"
+  },
+  {
+    "revision": "981ff59c172bf05e9a606988718703e5",
+    "url": "/poker-simulator-react/static/media/background.981ff59c.png"
+  },
+  {
+    "revision": "12aee8a94d0da517fa57eedcd848f03e",
+    "url": "/poker-simulator-react/static/media/paus_2.12aee8a9.png"
+  }
+]);
